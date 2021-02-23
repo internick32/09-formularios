60aa096c0c84ee9ab35a804f3e8e08ad7c0c1a6a
Trigalo27 <internick27@gmail.com>
2021-02-23 11:59:07 -0600

    comit a 09-formularios

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre: 'Jose Trinidad',
+    apellido: 'Garcia Lopez',
+    correo: 'internick10@gmail.com',
+    pais: 'MEX',
+    genero: 'M'
+  }
+
+  paises: any[] = [];
+
+  constructor( private paisService: PaisService ) { }
+
+  ngOnInit() {
+    this.paisService.getPaises()
+    .subscribe( paises => {
+      this.paises = paises;
+      this.paises.unshift({ nombre: '[Seleccione un pais]', codigo: '' });
+      console.log( this.paises );
+    });
+  }
+
+  guardar( forma: NgForm){
+
+    if( forma.invalid ) {
+
+      Object.values( forma.controls ).forEach( control => {
+        control.markAsTouched();
+      });
+
+      return;
+    }
+
+    console.log('El metodo guardar disparado...');
+    console.log( forma );
+    console.log( forma.value );
+  }
+
+}
